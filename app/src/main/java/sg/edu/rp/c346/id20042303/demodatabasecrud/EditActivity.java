8a8a6325a84ad95f74711e79b0e7c1f8593c326d
Tay Hao Wen <tay_haowen@hotmail.com>
2021-07-13 12:14:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20042303/demodatabasecrud/EditActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id20042303.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        tvID = findViewById(R.id.tvID);
+        etContent = findViewById(R.id.etEditContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+                Toast.makeText(EditActivity.this,"Update Successful",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                finish();
+            }
+        });
+
+    }
+}
